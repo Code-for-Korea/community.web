c904502fb70a3e7bdc7ec2936315fc896a6e7d0d
Keriel <keriel@jupiterflow.com>
2020-05-16 16:11:08 +0900

    :sparkles: update nav-bar
    * add mobile(max-width: 991px, 479px) nav menu

--- static/js/animation.js
+++ static/js/animation.js
@@ -10,4 +10,22 @@ window.addEventListener('load', () => {
             top: targetNode.offsetTop - 64
         });
     });
+    document.getElementsByClassName('nav-menu-icon')[0].addEventListener('click', (evt) => {
+        const targetIco = evt.target;
+        const navOverlay = document.getElementsByClassName('nav-overlay')[0];
+        const navLinkMenu = document.querySelector('.nav-overlay > .nav-link-menu');
+
+        if(targetIco.className === 'nav-menu-icon') {
+            targetIco.className = 'nav-menu-icon on';
+            navOverlay.className = 'nav-overlay on';
+            navLinkMenu.className = 'nav-link-menu on';
+        }
+        else {
+            targetIco.className = 'nav-menu-icon';
+            navOverlay.className = 'nav-overlay';
+            navLinkMenu.className = 'nav-link-menu';
+        }
+
+
+    });
 });
